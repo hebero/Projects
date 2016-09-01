4b18f22de6b4d7c485daddb0cd081ad32f3a7ece
Heber <heberdanielorellana@gmail.com>
2016-09-01 08:04:10 -0600

    Inicial

---
+++ CrediSeguroWebw/CrediSeguro2/Afiliadodb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CrediSeguro2
+{
+    public class Afiliadodb
+    {
+        public int Insert(string Empresa, string Contacto, string SQLConexion)
+        {
+
+        }
+    }
+}
